d3d4653ac15b2d0f69c6494c1d51583b7c45cd70
sj <sujung033131@gmail.com>
2023-07-01 23:57:38 +0900

    0701 Fourth commit

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.board.dto.BoardDTO;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -16,13 +17,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Controller
 @Log4j2
 @RequestMapping("/board/")
+@RequiredArgsConstructor
 public class BoardController {
 
     // 목록
     @GetMapping("list")
-    public void List(){
+    public void List(Model model){
 
         log.info("list.......");
+
+        List<BoardDTO> list = boardService.boardList();
+
+		model.addAttribute("board", list);
         
     }
 
@@ -38,15 +44,21 @@ public class BoardController {
     @PostMapping("regist")
     public String postRegist(BoardDTO boardDTO){
 
+        boardService.regist(boardDTO);
+
         return "redirect:/board/list";
     }
 
 
     // 조회
     @GetMapping("read/{tno}")
-    public String getRead(@PathVariable("tno") Long tno){
+    public String getRead(@PathVariable("tno") Long tno, Model model){
 
         log.info("GET read...........");
+
+        BoardDTO dto = boardService.read(tno);
+
+		model.addAttribute("read", dto);
 
         return "/board/read";
     }
@@ -55,6 +67,8 @@ public class BoardController {
     // 삭제
     @PostMapping("delete/{tno}")
     public String postDelete(@PathVariable("tno") Long tno){
+
+        boardService.delete(tno);
         
         return "redirect:/board/list";
     }
@@ -62,15 +76,21 @@ public class BoardController {
 
     // 수정
     @GetMapping("modify/{tno}")
-    public String getModify(@PathVariable("tno") Long tno){
+    public String getModify(@PathVariable("tno") Long tno, Model model){
 
         log.info("GET Modify..........");
+
+        BoardDTO dto = boardService.read(tno);
+
+		model.addAttribute("modify", dto);
 
         return "/board/modify";
     }
 
     @PostMapping("modify/{tno}")
-    public String postModify(@PathVariable("tno") Long tno){
+    public String postModify(@PathVariable("tno") Long tno, BoardDTO boardDTO){
+
+        boardService.modify(boardDTO);
 
         return "redirect:/board/read/{tno}";
     }
